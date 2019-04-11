4db4b2d052d3f89dda50caf81b15dded645ca1e2
bevvvvv <computerwhiz1337@gmail.com>
2019-04-11 14:34:34 -0400

    Google vision script

---
+++ April/scanImages.py
@@ -0,0 +1,40 @@
+import googleVision
+import os
+import csv
+
+pictures = os.listdir(os.path.join(os.path.dirname(__file__), 'resources/'))
+picLabels = {}
+picText = {}
+
+for i in range(len(pictures)):
+    name = 'Post ' + str(i + 1)
+    print(name)
+    picLabels[name] = []
+    picText[name] = []
+    #if i == 1:
+    #    break
+    imagePath = googleVision.image_path_here(pictures[i])
+    # label text in labels[index].description
+    labels = googleVision.detect_label(imagePath)
+    for x in labels:
+        picLabels[name].append(x.description)
+    # text string in text[index].description
+    texts = googleVision.detect_text(imagePath)
+    for y in texts:
+        picText[name].append(y.description)
+
+## Write API Results to CSV
+with open(os.path.join(os.path.dirname(__file__),'picLabel.csv'), 'w') as csvFile:
+    writer = csv.writer(csvFile, delimiter=',')
+    for key in picLabels:
+        for i in range(len(picLabels[key])):
+            writer.writerow([key, picLabels[key][i]])
+with open(os.path.join(os.path.dirname(__file__),'picText.csv'), 'w') as csvFile:
+    writer = csv.writer(csvFile, delimiter=',')
+    for key in picText:
+        for i in range(len(picText[key])):
+            writer.writerow([key, picText[key][i]])
+                        
+
+#print(labels)
+#print(texts)
